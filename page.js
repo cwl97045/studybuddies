4d0cc09f4c6768fc3c54f8507e8837a536200e9c
Christopher Layton <laytonsunlimited@gmail.com>
2014-03-17 14:13:18 -0700

    Clouds generate randomly

--- page.js
+++ page.js
@@ -1,7 +1,15 @@
 $(document).ready(function (){
-  var clouds = makeCloudElements(Math.floor(Math.random()*11));
+  var clouds = makeCloudElements(Math.floor(Math.random()*11 + 20));
   for(var i = 0; i < clouds.length; i++){
+    var posx = Math.floor(Math.random()*1500 + 1);
+    var posy = Math.floor(Math.random()*700 + 1);
     $('#sky').append(clouds[i]);
+    $(clouds[i]).css({
+      'position': 'absolute',
+      'left' : posx + 'px',
+      'top' : posy + 'px',
+      'z-index' : '-1'
+      });
     drawCloud(clouds[i]);
   }
   drawGround();
