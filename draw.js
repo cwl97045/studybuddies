03157794784f7621319650735b3384c2f85fdfb1
Christopher Layton <laytonsunlimited@gmail.com>
2014-03-18 09:41:40 -0700

    Added left movement

--- draw.js
+++ draw.js
@@ -1,3 +1,10 @@
+var colors = {
+    grassGreen : '#49E20E',
+    outline : '#000000',
+    cloudWhite : '#ffffff'
+}
+
+
 function getRandomNum(min, max){
   return Math.floor(Math.random(max-min + 1)) + min;
 }
@@ -6,18 +13,17 @@ function getRandomNum(min, max){
 function makeCloudElements(amount){
   var clouds = [];
   for(var i = 0; i < amount; i++){
-    var cloud = $('<canvas />').attr({id: "canvas" + i, width: '300px', height:'100px', class: 'see cloudMoveRight'});
+    var cloud = $('<canvas />').attr({id: "canvas" + i, width: '300px', height:'100px'});
     clouds.push(cloud);
   }
   return clouds;
 }
 function drawGround(){
   var canvas = document.getElementById('ground');
-  var grassGreen = '#49E20E';
   if(canvas){
     var ctx = canvas.getContext('2d');
-    ctx.strokeStyle = grassGreen;
-    ctx.fillStyle = grassGreen;
+    ctx.strokeStyle = colors.grassGreen;
+    ctx.fillStyle = colors.grassGreen;
     ctx.lineJoin = 'round';
     ctx.beginPath();
     ctx.moveTo(0,0);
@@ -33,20 +39,34 @@ function drawGround(){
     ctx.lineTo(1200, 20);
     ctx.lineTo(1300, 15);
     ctx.lineTo(1500, 0);
-    ctx.lineTo(1500, 200);
-    ctx.lineTo(0,200);
+    ctx.lineTo(1500, 400);
+    ctx.lineTo(0,400);
     ctx.stroke();
     ctx.fill();
+    ctx.strokeStyle = colors.outline;
+    ctx.moveTo(0,0);
+    ctx.lineTo(100,20);
+    ctx.lineTo(200,50);
+    ctx.lineTo(300,40);
+    ctx.lineTo(400, 40);
+    ctx.lineTo(500,30);
+    ctx.lineTo(700, 30);
+    ctx.lineTo(800, 40);
+    ctx.lineTo(900, 30);
+    ctx.lineTo(1000,30);
+    ctx.lineTo(1200, 20);
+    ctx.lineTo(1300, 15);
+    ctx.lineTo(1500, 0);
+    ctx.stroke();
   }
 }
 
 function drawCloud(canvasElm){
   var canvas = canvasElm[0];
-  var cloudWhite = '#ffffff';
   if(canvas){
     var ctx = canvas.getContext('2d');
-    ctx.strokeStyle = cloudWhite;
-    ctx.fillStyle = cloudWhite;
+    ctx.strokeStyle = colors.cloudWhite;
+    ctx.fillStyle = colors.cloudWhite;
     ctx.beginPath();
     ctx.moveTo(10,50);
     ctx.bezierCurveTo(12,40,20,30,39,30);
@@ -57,6 +77,16 @@ function drawCloud(canvasElm){
     ctx.bezierCurveTo(220,30,250, 10, 260, 50);
     ctx.lineTo(10,50);
     ctx.fill();
+    ctx.stroke();
+    ctx.strokeStyle = colors.outline;
+    ctx.moveTo(10,50);
+    ctx.bezierCurveTo(12,40,20,30,39,30);
+    ctx.bezierCurveTo(39,20,60,5,80,20);
+    ctx.bezierCurveTo(90,2,115,2,130,20);
+    ctx.bezierCurveTo(140,2,175,2,180,20);
+    ctx.bezierCurveTo(190,10,220,10,220,30);
+    ctx.bezierCurveTo(220,30,250, 10, 260, 50);
+    ctx.lineTo(10,50);
     ctx.stroke();
   }
 }
--- page.js
+++ page.js
@@ -1,8 +1,26 @@
+function cloudCheck(){
+  $('canvas.cloudMoveRight').each(function(){
+    var position = this.layerX;
+    if(position > 1500){
+      this.css({
+        'left' : '-200px'});
+    }
+  });
+  
+  $('canvas.cloudMoveLeft').each(function(){
+    
+  });
+}
+
 $(document).ready(function (){
-  var clouds = makeCloudElements(Math.floor(Math.random()*11 + 20));
+  var clouds = makeCloudElements(Math.floor(Math.random()*11 + 10));
   for(var i = 0; i < clouds.length; i++){
     var posx = Math.floor(Math.random()*1500 + 1);
     var posy = Math.floor(Math.random()*700 + 1);
+    var animation = 'cloudMoveRight';
+    if(posx % 2 !== 0){
+      animation = 'cloudMoveLeft';
+    }
     $('#sky').append(clouds[i]);
     $(clouds[i]).css({
       'position': 'absolute',
@@ -10,8 +28,12 @@ $(document).ready(function (){
       'top' : posy + 'px',
       'z-index' : '-1'
       });
+    $(clouds[i]).addClass(animation);
     drawCloud(clouds[i]);
   }
+  
+  setInterval(function(){cloudCheck()}, 5000);
+  
   drawGround();
   $('input[type="submit"]').click(function(){
     var username = $('input[type="text"]').val();
